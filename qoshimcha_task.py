20f05369228396e414935b5c3fff50042cb37df6
ubparviz <ubparviz@gmail.com>
2025-05-31 23:42:49 +0500

    Done

--- qoshimcha_task.py
+++ qoshimcha_task.py
@@ -7,3 +7,7 @@ find_num = text.count("1") + text.count("2") + text.count("3") + text.count("4")
 print(f"Raqamlar soni: {find_num}")
 
 # Hali yangi funksiyalar o'tilmagani uchun bu kodni qisqartiromadim
+
+# Algaritm qanday ishlaydi?
+# Masalan INPUT : 2025 -- OUTPUT: 4 (4 raqam bor degani)
+# Xullas matnda nechta raqam bor hammasini sanab beradi
